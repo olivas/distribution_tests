274ead74c02351b8fd98dddaedb7d5128a92c360
Alex Olivas <alex.r.olivas@gmail.com>
2017-08-23 11:57:18 -0600

    added the controversial LLH value test.  who knows if it'll be useful.

---
+++ python/compare/tests/llh_value.py
@@ -0,0 +1,18 @@
+from math import log
+from scipy.special import binom
+# Test from F.Porter "Testing Consistency of Two Histograms" arXiv : 0804.0380# 
+
+def llh_value(h1, h2):
+    r"""
+    Compare histograms h1, h2 with the log likelihood ratio test.
+    """
+    result = 0.
+    Nu = sum(h1.bin_values)
+    Nv = sum(h2.bin_values)
+    for u,v in zip(h1.bin_values, h2.bin_values):
+        t = u + v
+        term1 = log(binom(t,v))
+        term2 = t*log(Nu/(Nu + Nv))
+        term3 = v*log(Nv/Nu)
+        result += term1 + term2 + term3
+    return -result
